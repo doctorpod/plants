377cb7d3ea61597813d7859999f193e240a1e10d
Andy <andy@wireworldmedia.co.uk>
2020-05-05 17:00:10 +0100

    Refactor percentage germinated to model method

--- app/models/sowing.rb
+++ app/models/sowing.rb
@@ -1,3 +1,9 @@
 class Sowing < ApplicationRecord
   belongs_to :seed
+
+  def percentage_germinated
+    return nil if num_germinated.blank?
+
+    "#{(100 * (num_germinated / num_sown.to_f)).round}%"
+  end
 end
