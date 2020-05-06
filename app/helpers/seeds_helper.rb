a697a545cb39243f69d18f23c8014d959f124878
Andy <andy@wireworldmedia.co.uk>
2020-05-06 21:08:16 +0100

    Apply styling to sowing months

--- app/helpers/seeds_helper.rb
+++ app/helpers/seeds_helper.rb
@@ -2,13 +2,15 @@ module SeedsHelper
   def sowing_months(month_numbers_string)
     return '' if month_numbers_string.blank?
 
-    months = %w[j f m a m j j a s o n d]
+    month_letters = %w[J F M A M J J A S O N D]
     month_numbers = month_numbers_string.split(' ').map(&:to_i).uniq
+    out = []
 
-    month_numbers.each do |month_number|
-      months[month_number - 1] = months[month_number - 1].upcase
+    month_letters.each_with_index do |month_letter, i|
+      span_class = month_numbers.include?(i + 1) ? 'month-bright' : 'month-dim'
+      out << content_tag(:span, month_letter, class: span_class)
     end
 
-    months.join(' ')
+    out.join.html_safe
   end
 end
